9a1ff24af14119231863247610ee49587d3753ef
djx501 <bhargavdhananjaix501@gmail.com>
2026-04-08 23:35:08 +0530

    joker

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("This is for testing purposes that live code is working or joker!");
+    res.send("This is for testing  that live code is working or joker!");
 });
 
 module.exports = router;
